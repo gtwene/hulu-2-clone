f0e53b1476caffc14d69c0a1548d66c056b3a6f5
Kwame <gtwene001@st.ug.edu.gh>
2021-11-09 16:08:57 +0000

    cssGrid

--- components/Thunbnail.js
+++ components/Thunbnail.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
+import { forwardRef } from "react";
 
-function Thunbnail({ result }) {
+const Thunbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
-    <div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-150 hover:z-50">
+    <div
+      ref={ref}
+      className="group cursor-pointer p-2 transition duration-100 ease-in transform sm:hover:scale-105 hover:z-50"
+    >
       <Image
         layout="responsive"
         src={
@@ -30,6 +34,6 @@ function Thunbnail({ result }) {
       </div>
     </div>
   );
-}
+});
 
 export default Thunbnail;
