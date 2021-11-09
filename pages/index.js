d2cfb2615c2368c5adac41c62564f3a2856b2193
Kwame <gtwene001@st.ug.edu.gh>
2021-11-09 13:43:14 +0000

    HeaderComponent

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -10,7 +11,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Lets Build Hulu 2.0</h1>
+      {/* Header */}
+      <Header />
+
+      {/* Nav */}
+
+      {/* Results */}
     </div>
   );
 }
